7562c43e35292277d7249313503e347921769742
vikrant-29 <vikrantpatil1104@gmail.com>
2025-07-11 17:57:50 +0530

    task 1 added

---
+++ task1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ApexPlanet Intern</title>
+</head>
+<body>
+    
+</body>
+</html>
+
+<?php
+echo "Hello <h1> ApexPlanet </h1>";
+?>
